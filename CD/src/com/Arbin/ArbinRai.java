6ce9abd2cc7d14bd74a9f6363105cb3fed9b148f
Arbin <Arbinkrai@gmail.com>
2015-10-14 13:31:15 -0500

    added dinosaur class and also added dinosaurSupervisor in ArbinRai class

--- CD/src/com/Arbin/ArbinRai.java
+++ CD/src/com/Arbin/ArbinRai.java
@@ -12,5 +12,27 @@ public void claculateCosts(int price){
     System.out.println("The total price of the selected CDs and LPs is "+grandTotal+".");
 
 }
-}
+
+    //public class DinosaurSupervisor {
+
+        public static void main(String[] args) {
+
+            //Create dinosaur objects
+            Dinosaur trex = new Dinosaur();
+            Dinosaur velociraptor = new Dinosaur();
+            Dinosaur diplodocus = new Dinosaur();
+
+            //Call roar methods
+            trex.roar();
+            velociraptor.roar();
+            diplodocus.roar();
+
+            //Test color attribure of dinosaur object
+            trex.color = "blue";
+            System.out.println("A T-Rex is " + trex.color);
+        }
+    }
+
+
+
 
---
+++ CD/src/com/Arbin/Dinosaur.java
@@ -0,0 +1,20 @@
+package com.Arbin;
+
+/**
+ * Created by lt3733yt on 10/14/2015.
+ */
+public class Dinosaur {
+
+
+
+
+        //Color attribute
+        String color;
+
+        public void roar(){
+            System.out.println("GRRRRR!!!");
+        }
+
+    }
+
+
